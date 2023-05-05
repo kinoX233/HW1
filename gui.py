ab0f14b12af3636d063506df75812d66b5f26001
kino <2493527648@qq.com>
2023-05-05 20:19:46 +0800

    gui is opreational now, can create a mainwindow and a subwindow after clicked button api and data dispose requested

---
+++ gui.py
@@ -0,0 +1,83 @@
+import api
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+# mainwindow for input
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(840, 578)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.textEdit = QtWidgets.QTextEdit(self.centralwidget)
+        self.textEdit.setGeometry(QtCore.QRect(190, 220, 391, 41))
+        self.textEdit.setObjectName("textEdit")
+        self.label = QtWidgets.QLabel(self.centralwidget)
+        self.label.setGeometry(QtCore.QRect(230, 130, 311, 71))
+        font = QtGui.QFont()
+        font.setFamily("Microsoft YaHei UI")
+        font.setPointSize(16)
+        self.label.setFont(font)
+        self.label.setObjectName("label")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(300, 280, 171, 51))
+        font = QtGui.QFont()
+        font.setFamily("Arial")
+        font.setPointSize(12)
+        font.setBold(True)
+        font.setWeight(75)
+        self.pushButton.setFont(font)
+        self.pushButton.setObjectName("pushButton")
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 840, 26))
+        self.menubar.setObjectName("menubar")
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QtWidgets.QStatusBar(MainWindow)
+        self.statusbar.setObjectName("statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "nuclear weapon"))
+        self.label.setText(_translate("MainWindow", "type your target IP here"))
+        self.pushButton.setText(_translate("MainWindow", "launch weapon!"))
+
+#subwindow for output
+class Ui_Dialog(object):
+    def setupUi(self, Dialog):
+        Dialog.setObjectName("Dialog")
+        Dialog.resize(832, 578)
+        font = QtGui.QFont()
+        font.setPointSize(12)
+        Dialog.setFont(font)
+        self.label = QtWidgets.QLabel(Dialog)
+        self.label.setGeometry(QtCore.QRect(30, 30, 91, 21))
+        self.label.setObjectName("label")
+        self.textEdit = QtWidgets.QTextEdit(Dialog)
+        self.textEdit.setGeometry(QtCore.QRect(130, 20, 201, 41))
+        self.textEdit.setObjectName("textEdit")
+        self.label_2 = QtWidgets.QLabel(Dialog)
+        self.label_2.setGeometry(QtCore.QRect(30, 100, 91, 21))
+        self.label_2.setObjectName("label_2")
+        self.textEdit_2 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_2.setGeometry(QtCore.QRect(130, 90, 391, 41))
+        self.textEdit_2.setObjectName("textEdit_2")
+        self.label_3 = QtWidgets.QLabel(Dialog)
+        self.label_3.setGeometry(QtCore.QRect(30, 170, 91, 21))
+        self.label_3.setObjectName("label_3")
+        self.textEdit_3 = QtWidgets.QTextEdit(Dialog)
+        self.textEdit_3.setGeometry(QtCore.QRect(130, 180, 391, 281))
+        self.textEdit_3.setObjectName("textEdit_3")
+
+        self.retranslateUi(Dialog)
+        QtCore.QMetaObject.connectSlotsByName(Dialog)
+
+    def retranslateUi(self, Dialog):
+        _translate = QtCore.QCoreApplication.translate
+        Dialog.setWindowTitle(_translate("Dialog", "boom!"))
+        self.label.setText(_translate("Dialog", "IP"))
+        self.label_2.setText(_translate("Dialog", "location"))
+        self.label_3.setText(_translate("Dialog", "wheather"))
---
+++ main.py
@@ -0,0 +1,24 @@
+import gui
+import sys
+from PyQt5.QtWidgets import QMainWindow, QDialog, QApplication
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    #creat mainwindow
+    mainwindow = QMainWindow()
+    main_ui = gui.Ui_MainWindow()
+    main_ui.setupUi(mainwindow)
+
+    #create subwindow
+    subwindow = QDialog()
+    sub_ui = gui.Ui_Dialog()
+    sub_ui.setupUi(subwindow)
+
+    #after clicked, subwindow shows
+    btn = main_ui.pushButton
+    btn.clicked.connect( subwindow.show )#no parentheses() for function
+
+    #show mainwindow or quit
+    mainwindow.show()
+    #subwindow.show()
+    sys.exit(app.exec_())
