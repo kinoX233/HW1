680b518b1ddf12a72ed176fab444c625ff038374
kino <2493527648@qq.com>
2023-05-06 17:48:54 +0800

    info print sucessful now weather request needed met problem with api.py and main.py copy api.py to main.py as solution api.py abandoned now

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
 import sys
-from api import *
 from gui import *
 from PyQt5.QtWidgets import QMainWindow, QDialog, QApplication
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
+import json
+import requests
+IP_URL = "http://api.liangmlk.cn?ak=xbDpKsZJcFgmSyLlAXHo&appid=602&ip="
 
+
+def main():
+    
+    class atom (object):  # class for all the info about target location
+
+        def __init__(self):
+            self.ip = ""
+            self.country = ""
+            self.province = ""
+            self.city = ""
+            self.weather = ""
+            self.type = ""
+
+        def get_loc(self, ip):  # use ip to get the location
+            self.ip = ip
+
+            post = IP_URL + self.ip
+            loc_json = requests.get(post)
+
+            loc = json.loads(loc_json.text)["result"]
+            # print(loc)
+            self.country = loc["country"]
+            self.province = loc["province"]
+            self.city = loc["city"]
+            self.type = loc["type"]
+
+        def show_info(self):#show all info to dialog
+            location = f'country:{atom.country}   province:{atom.province}  city:{atom.city}'
+            wheather = f''
+
+            sub_ui.text_ip.setText(atom.ip)
+            sub_ui.text_loc.setText(location)
+            sub_ui.text_weather.setText(wheather)
+    
+    app = QApplication(sys.argv)
     #creat mainwindow
     mainwindow = QMainWindow()
     main_ui = Ui_MainWindow()
@@ -15,6 +50,9 @@ if __name__ == '__main__':
     sub_ui = Ui_Dialog()
     sub_ui.setupUi(subwindow)
 
+    #show mainwindow
+    mainwindow.show()
+
     #after clicked, subwindow shows
     btn = main_ui.pushButton
     btn.clicked.connect( subwindow.show )#no parentheses() for function
@@ -23,7 +61,11 @@ if __name__ == '__main__':
     atom = atom()
     btn.clicked.connect( lambda: atom.get_loc(main_ui.textEdit.toPlainText()) )
 
-    #show mainwindow or quit
-    mainwindow.show()
+    #show info
+    btn.clicked.connect( atom.show_info )
+
+    #quit
     sys.exit(app.exec_())
 
+if __name__ == "__main__":
+    main()
